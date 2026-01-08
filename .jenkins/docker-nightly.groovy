7769c67dd4b9552d44aaef9d3a3fb3879bb13d30
Mattia Dal Ben <mattdibi@users.noreply.github.com>
2026-01-08 13:04:02 +0100

    ci(docker): docker nightly build (#8)
    * ci(docker): docker nightly build
    
    * fix: build
    
    * ci: weekly run
    
    * style: remove extra newline
    
    * feat: disable annotations

---
+++ .jenkins/docker-nightly.groovy
@@ -0,0 +1,33 @@
+@Library('releng-pipeline') _
+
+timeout(time:90, unit:'MINUTES') {
+    podTemplate(yaml: loadOverridableResource(libraryResource: 'org/eclipsefdn/container/agent.yml')) {
+        node(POD_LABEL) {
+            properties([
+                buildDiscarder(logRotator(numToKeepStr: '5')),
+                disableConcurrentBuilds(),
+                pipelineTriggers([
+                    cron('H 0 * * 6') // Run on Saturday night
+                ])
+            ])
+
+            checkout scm
+
+            withEnv(["HOME=${env.WORKSPACE}"]){
+                stage('build') {
+                    dir('docker') {
+                        container('containertools') {
+                            containerBuild(
+                                annotation: false,
+                                credentialsId: 'docker-bot-token',
+                                name: 'docker.io/eclipsekura/kura',
+                                version: 'nightly',
+                                dockerfile: 'Dockerfile.debian'
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
